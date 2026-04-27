3741c4a4aa1b1dbe8f50c45a7485d607301aaebf
brayam2000max <brayam2000max.222@gmail.com>
2026-04-26 22:21:00 -0500

    Docker

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DatabaseService } from './database/database.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const db = app.get(DatabaseService);
+  if (db.isEnabled()) {
+    await db.ensureReady();
+  }
   app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
